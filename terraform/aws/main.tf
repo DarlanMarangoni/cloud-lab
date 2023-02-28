5d00805dd1bb1f716232e22504e92d0ca01c9180
Darlan Marangoni <marangoni.darlan1@gmail.com>
2023-02-28 13:37:13 -0300

    criando EC2

---
+++ terraform/aws/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  #  version = "~> 2.0"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-026c8acd92718196b"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev${count.index}"
+  }
+}
